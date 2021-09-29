3fc43250dc2084343951da00721219f0bf0dee27
Md Shariful islam <shar51338@gmail.com git config --global user.name shariff61>
2021-09-29 20:50:53 +0300

    starting learn js

---
+++ hello.js
@@ -0,0 +1,161 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+var guest = 200;
+console.log(guest);
+var makingNewHotel = 5;
+console.log(makingNewHotel);
+var inTheMorning = 'good Morning.now 8am.';
+console.log(inTheMorning)
+var whenISeeMyFrind = 'hey how are you today';
+console.log(whenISeeMyFrind);
+
+var guest = 1200;
+console.log(typeof guest)
+var peoples = 24;
+console.log(typeof peoples);
+var myName = "sharif ul islam";
+console.log(typeof myName);
+
+var promise = "i promise i will learn web development In Sha Allah";
+console.log(promise.indexOf('learn'));
+
+var myName = "sharif";
+console.log(typeof myName);
+var myHotelName = "concorde resort&casino";
+console.log(typeof myHotelName);
+
+var isHot = true;
+console.log(typeof isHot);
+var isRich = false;
+console.log(typeof isRich);
+var isRich = false;
+console.log(typeof isRich);
+
+var friends = 40;
+console.log(typeof friends);
+var myFriendName = "tarek ul islam";
+console.log(typeof myFriendName);
+
+var promise = "i promise i will learn web development In Sha Allah";
+console.log(promise.indexOf('will'));
+var readyForSchool = "i am going to school for learn . thats why i am ready fo go school";
+console.log(readyForSchool.indexOf('school'));
+
+var schoolName = "my school name is gouripur s.a high school";
+console.log(schoolName.split('name'));
+
+var myHotelName = "kaya artemis resort&casino";
+console.log(myHotelName.split(' '));
+
+var myAdress = "my name is sharif. my vilage name is gopalpur";
+console.log(myAdress.indexOf('sharif'));
+console.log(myAdress.split('is'));
+console.log(typeof myAdress);
+console.log(myHotelName);
+
+var number1 = 25;
+var number2 = '15.5';
+number2 = parseFloat(number2)
+console.log(number1 + number2);
+
+var number1 = '49';
+number2 = 45.4;
+number1 = parseFloat(number1);
+console.log(number1 + number2);
+number1 = 22;
+number2 = 34.3;
+number2 = parseInt(number2);
+console.log(number1 + number2);
+number1 = 67.45;
+number2 = '44';
+number1 = parseFloat(number1);
+console.log(number1 + number2);
+number1 = 0.1;
+number2 = 0.2;
+var total = number1 + number2;
+total = total.toFixed(3);
+console.log(total);
+
+number1 = 0.6;
+number2 = 1.9;
+var total = number1 + number2;
+total = total.toFixed(4);
+console.log(total);
+
+number4 = 20.1;
+number5 = 3.2;
+var total = number4 + number5;
+total = total.toFixed(3);
+console.log(total);
+var name = '22.4';
+number = 24.01;
+
+var total = name + number;
+total = total.toFixed(3);
+console.log(total);
+
+
+var rahimClassRoll = 5;
+myClassRoll = 3;
+var total = rahimClassRoll + myClassRoll;
+console.log(total);
+
+var mangoPrice = 200;
+bananaPrice = 130;
+var total = mangoPrice - bananaPrice;
+console.log(total);
+var kayaHotelBooking = 1200;
+concordeHotelBooking = 1100;
+var total = kayaHotelBooking * concordeHotelBooking;
+console.log(total);
+var math = 90;
+english = 69;
+var result = math / english;
+result = result.toFixed(4);
+console.log(result);
+
+var math = 87;
+accounting = 77;
+var result = math % accounting;
+console.log(result);
+
+var price1 = 100;
+price2 = 200;
+price2 = price2 - 10;
+console.log(price2);
+var car1 = 6;
+car2 = 400;
+car2--;
+console.log(car2);
+
+var sharif = 44;
+arif = 33;
+var sharif = sharif + 20 - 10;
+console.log(sharif);
+
+var orange = -55.6;
+var orangeAbsulate = Math.abs(orange);
+console.log(orangeAbsulate);
+var mango = -33;
+var absulateMango = Math.abs(mango);
+console.log(absulateMango);
+var banana = 55.4;
+var result = Math.round(banana);
+console.log(result);
+
+var apple = 33.45;
+var total = Math.ceil(apple);
+console.log(total);
+var apple = 44.934;
+var apple = Math.floor(apple);
+console.log(apple);
+var sweet = Math.random() * 100;
+var priceTotal = Math.round(sweet);
+console.log(priceTotal);
+
+
+
+
+
+
+
